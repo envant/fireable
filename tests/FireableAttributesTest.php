d0564753bb465ecbaff0ca309d0059c48c415199
Ilia Lazarev <ilia.lazarev@icloud.com>
2022-02-19 10:11:17 +0300

    Support Laravel 9

--- tests/FireableAttributesTest.php
+++ tests/FireableAttributesTest.php
@@ -5,24 +5,29 @@ namespace Envant\Fireable\Tests;
 use Envant\Fireable\Tests\Events\EmailUpdated;
 use Envant\Fireable\Tests\Events\StatusApproved;
 use Envant\Fireable\Tests\Models\User;
+use Illuminate\Support\Facades\Event;
 
 class FireableAttributesTest extends TestCase
 {
     public function testSetAnyValue()
     {
-        $this->expectsEvents(EmailUpdated::class);
+        Event::fake(EmailUpdated::class);
 
         $this->testUser->update([
             'email' => 'appleseed@example.net',
         ]);
+
+        Event::assertDispatched(EmailUpdated::class);
     }
 
     public function testSetExpectedValue()
     {
-        $this->expectsEvents(StatusApproved::class);
+        Event::fake(StatusApproved::class);
 
         $this->testUser->update([
             'status' => User::STATUS_APPROVED,
         ]);
+
+        Event::assertDispatched(StatusApproved::class);
     }
 }
